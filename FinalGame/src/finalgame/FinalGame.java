ddd516cb4ca206fd4de9b0e9e4aab078c6956e88
AlexBahsler <55402414+AlexBahsler@users.noreply.github.com>
2020-01-16 12:04:23 -0600

    Add files via upload

---
+++ FinalGame/src/finalgame/FinalGame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package finalgame;
+
+/**
+ * FinalGame.java - description
+ *
+ * @author Alex Bahsler
+ * @since Jan. 15, 2020, 11:58:29 a.m.
+ */
+public class FinalGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UserInterface.main(args);
+    }
+
+}

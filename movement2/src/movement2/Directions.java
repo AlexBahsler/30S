b25b60c74de59a142f4644808316669a70423ce7
AlexBahsler <55402414+AlexBahsler@users.noreply.github.com>
2020-01-21 12:03:41 -0600

    Add files via upload

---
+++ movement2/src/movement2/Directions.java
@@ -0,0 +1,24 @@
+
+/** required package class namespace */
+package movement2;
+
+
+/**
+ * Directions.java - directional constants for movement. The use of "static" 
+ * means we can use the constants without creating an object of this class. 
+ * These are used in the GameCharacter class.
+ *
+ * @author Mr. Wachs 
+ * @since Jan. 8, 2020, 8:43:53 a.m.
+ */
+public class Directions 
+{
+    
+    public static final int STOP  = 0;
+    public static final int LEFT  = 1;
+    public static final int RIGHT = 2;
+    public static final int UP    = 3;
+    public static final int DOWN  = 4;
+            
+    public static final int ROLL = 5;
+}
